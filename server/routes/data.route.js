3ff22609191769f1abbda1a973b40e2541409e68
imHaris8278 <harisbaig7081@gmail.com>
2024-08-18 05:42:52 +0500

    Update data.route.js

--- server/routes/data.route.js
+++ server/routes/data.route.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { webScraper } from "../controllers/data.controller.js";
+import { getAllData, webScraper } from "../controllers/data.controller.js";
 const route = express.Router();
 
 route.post("/web-scrape", webScraper);
+route.get("/scraped-data", getAllData);
 
 export default route;
